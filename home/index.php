e4b00a2ab953cd4c7d69e592d7606f166107fa90
Samridh Tuladhar <t.samridh.86@gmail.com>
2016-09-24 16:31:33 +0530

    made the home page

--- home/index.php
+++ home/index.php
@@ -20,36 +20,64 @@
 		</div>
 			<ul class="nav navbar-nav">
 				<li class="active"><a><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a></li>
-				<li><a><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Following </a></li>
+				<li><a><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Following </a></li>
 			</ul>
 			<form class="navbar-form navbar-left">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search for...">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
       </form>
 			<ul class="nav navbar-nav navbar-right">
+				<li><a href="logout"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Write a Post</a></li>
 				<li><a href="logout"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Log Out</a></li>
 			</ul>
 	</div>
 </nav>
+<div class="container-fluid">
+	
 <div class="row outer">
-	<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 profile">
+	<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+	<div class="profile">
 	<div class="row">
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<span class="heading bold"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Your profile</span>
 		<img src="../images/Wallpaper.jpg" class="img-responsive photoHolder" alt="Cinque Terre" height="100" width="100">
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 textHolder">
-			<p class="bold"> tsamridh86 </p>
-			<p> Samridh Tuladhar</p>
+			<p class="bold"> userName </p>
+			<p> firstName lastName</p>
+			<form method="post" action="editPage.php">
+			<input type="hidden" name="userName" value="userName"> 
+			<button type="button" class="btn btn-default">Edit Profile</button>
+			</form>
 		</div>
 	</div>
 	</div>
-	<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-		
+	<div class="profile">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<span class="heading bold"><span class="glyphicon glyphicon-fire" aria-hidden="true"></span> Now Trending</span><br>
+		posts
+		</div>
 	</div>
+	</div>
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-offset-1 col-md-8 col-lg-offset-1 col-lg-8 content"> 
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 repeat">
+				test	
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 repeat">
+				test	
+			</div>
+		</div>
+	</div>
+</div>
 </div>
 </body>
 </html>
