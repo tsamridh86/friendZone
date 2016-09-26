2e3d46861d2b572e157fb73f911f942a3f59a36e
Samridh Tuladhar <t.samridh.86@gmail.com>
2016-09-26 16:33:49 +0530

    started editProfile

---
+++ home/editProfile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit your profile</title>
+	<!-- links to bootstrap & jQuery -->
+	<script src="../js/jquery-3.1.0.min.js"></script>
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/bootstrap-theme.min.css"> 
+	<script src="../js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/editProfile.css">
+
+</head>
+<body>
+<div class="row">
+	<div class="col-xs-9 col-sm-9 col-md-3 col-lg-3">
+		<h3 class="heading"><span class="glyphicon glyphicon-erase" aria-hidden="true"></span> Edit Profile </h3>
+	</div>
+	<div class="row">
+		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 pull-right exit">
+			<a href="index.php"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Cancel</a>
+		</div>
+	</div>
+</div>
+<hr>
+</body>
+</html>
