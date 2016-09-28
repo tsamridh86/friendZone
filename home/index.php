819e5e48b244503e8c7a84a4cff9d512a7e569b6
Samridh Tuladhar <t.samridh.86@gmail.com>
2016-09-28 22:42:07 +0530

    completed search page

--- home/index.php
+++ home/index.php
@@ -37,7 +37,7 @@ $users->logout();
 			<li class="active"><a><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a></li>
 			<li><a><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Following </a></li>
 		</ul>
-		<form class="navbar-form navbar-left">
+		<form class="navbar-form navbar-left" method="get" action="search.php">
 			<input type="text" class="form-control" placeholder="Search for...">
           	<button type="submit" class="btn btn-default" ><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
         </form>
